1bb74ad8a8290fdca5ddc3bae5d41442a7fb64d5
Daniel Ipince <ipince22@gmail.com>
2021-03-31 22:55:41 +0000

    v1.0

--- src/main.py
+++ src/main.py
@@ -8,8 +8,11 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
-#from models import Person
+from models import db, Todo, User
+#JWT - SECURITY
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
+import datetime
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -20,6 +23,10 @@ db.init_app(app)
 CORS(app)
 setup_admin(app)
 
+# Setup the Flask-JWT-Extended extension
+app.config["JWT_SECRET_KEY"] = "super-secret"  # Change this!
+jwt = JWTManager(app)
+
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
@@ -29,15 +36,113 @@ def handle_invalid_usage(error):
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
+#login---------------------------------------------------------
+@app.route('/login', methods=["POST"])
+def login():
+    if request.method == "POST":
+        username = request.json["username"]
+        password = request.json["password"]
 
+        # Validate
+        if not username:
+            return jsonify({"error": "username Invalid"}), 400
+        if not password:
+            return jsonify({"error": "Password Invalid"}), 400
+        
+        user = User.query.filter_by(username=username).first()
+
+        if not user:
+            return jsonify({"error": "User not found"}), 400
+        
+        if not check_password_hash(user.password, password):
+            return jsonify({"error": "Wrong password"}), 400
+        
+        # Create Access Token
+        expiration_date = datetime.timedelta(days=1)
+        #expiration_date = datetime.timedelta(minutes=1)
+        access_token = create_access_token(identity=username, expires_delta=expiration_date)
+
+        request_body = {
+            "user": user.serialize(),
+            "token": access_token
+        }
+
+        return jsonify(request_body), 200
+
+# --User--------------------------------------------------------
 @app.route('/user', methods=['GET'])
-def handle_hello():
+def listarUsuarios():
+    users = User.query.all()
+    request = list(map(lambda user:user.serialize(),users))    
+    return jsonify(request), 200
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+@app.route('/user/<int:id>', methods=['GET'])
+def lista_usuario(id):
+    #user = User.query.get(id)
+    user = User.query.filter_by(id=id).first()
+    if user is None:
+        raise APIException("Message:No se encontro el user",status_code=404)
+    request = user.serialize()
+    return jsonify(request), 200
 
-    return jsonify(response_body), 200
+@app.route('/user', methods=["POST"])
+def crear_usuarios():
+    data = request.get_json()
+    hashed_password = generate_password_hash(data["password"],method='sha256')
+    user1 = User(username=data["username"],email=data["email"],password=hashed_password)
+    db.session.add(user1)
+    db.session.commit()
+    return jsonify("Message : Se adiciono un usuario!"),200
+
+@app.route('/user/<id>', methods=["DELETE"])
+@jwt_required()
+def delete_usuarios(id):
+    current_user = get_jwt_identity()
+    user1 = User.query.get(id)
+    if user1 is None:
+        raise APIException("usuario no existe!",status_code=404)
+    db.session.delete(user1)
+    db.session.commit()
+    return jsonify({"Proceso realizado con exito por el usuario:" : current_user}),200
+
+# --to do list--------------------------------------------------------
+@app.route('/todo', methods=['GET'])
+def listarTodos():
+    todos = Todo.query.all()
+    request = list(map(lambda todo:todo.serialize(),todos))    
+    return jsonify(request), 200
+
+@app.route('/todo/<int:id>', methods=['GET'])
+def listarItem(id):
+    #todo1 = Todo.query.get(id)
+    todo1 = Todo.query.filter_by(id=id).first()
+    if todo1 is None:
+        return APIException({"message": "No se encontro el item"},status_code=404)
+    request = todo1.serialize()
+    return jsonify(request), 200
+
+@app.route('/todo', methods=['POST'])
+def crearTodo():
+    return jsonify("Metodo Post"), 200
+
+@app.route('/todo', methods=['PUT'])
+def updateTodo():
+    return jsonify("Metodo PUT"), 200
+
+@app.route('/todo', methods=['DELETE'])
+def deleteTodo():
+    return jsonify("Metodo DELETE"), 200
+
+
+
+
+
+
+
+
+
+
+
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -4,9 +4,10 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(120), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    password = db.Column(db.String(500), unique=False, nullable=False)
+    
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -14,6 +15,24 @@ class User(db.Model):
     def serialize(self):
         return {
             "id": self.id,
+            "username": self.username,
             "email": self.email,
+            "password": self.password
+            # do not serialize the password, its a security breach
+        }
+        
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    done = db.Column(db.Boolean(), nullable=False)
+    label = db.Column(db.String(120), nullable=False)
+
+    def __repr__(self):
+        return '<Todo %r>' % self.label
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "label": self.label,
+            "done": self.done,
             # do not serialize the password, its a security breach
         }
